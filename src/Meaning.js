581f70fd3fb6e1001bd72399bc60a8e7ddda6b16
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-19 18:12:55 +0200

    added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  //console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
