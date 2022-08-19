3cad9284f72ae6b1a3bed0f7d9d2d6c57494a9b7
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-19 13:59:27 +0200

    added links to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -10,7 +10,33 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by mARTilda</small>
+          <small>
+            {" "}
+            This project is created by{" "}
+            <a
+              href="https://www.martilda.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              mARTilda
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/mARTilda-art/react-dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and is hosted on{" "}
+            <a
+              href="https://frolicking-treacle-3bb064.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
