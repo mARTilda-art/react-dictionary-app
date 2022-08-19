bacf64db7fa272760eb881d454fa17c327e737fa
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-19 13:19:48 +0200

    rendered API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-  function handleResponse(response) {}
+  function handleResponse(response) {
+    console.log(response.date[0]);
+    setResults(response.date[0]);
+    //console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
 
   function search(event) {
     event.preventDefault();
     /*alert(`Searching for ${keyword} definition`);*/
-
+    // documentation: https://api.dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -24,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
