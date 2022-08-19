14f9a2ecae135c203d71ff82282ba2c1a4da9eb3
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-19 17:55:45 +0200

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -7,9 +8,13 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <div>
         <p>
+          <strong>Definition: </strong>
           {props.meaning.definitions[0].definition}
           <br />
+          <strong>Example: </strong>
           <em>{props.meaning.definitions[0].example}</em>
+          <br />
+          <Synonyms synonyms={props.meaning.definitions[0].synonyms} />
         </p>
       </div>
     </div>
