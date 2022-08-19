48e1ee6fba1c7da93c9b0c895582c91fad9c529d
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-19 22:18:26 +0200

    added font awesome

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Phonetic.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        Listen{" "}
+        Listen <FontAwesomeIcon icon={faVolumeHigh}></FontAwesomeIcon>
       </a>
       <span className="text">{props.phonetic.text}</span>
     </div>
